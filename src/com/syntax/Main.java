f843418ffc147d55ee9546e5d0983ae1e1f599fd
angelicazaporozhets <angiezap95@gmail.com>
2022-01-26 18:56:05 -0600

    the red car is ready for the trial

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         // basic structure
         CarBasics car = new CarBasics();
+        car.color= "red";
+        car.engine = "799c";
+        car.door="2";
+        car.number="4567876";
+        System.out.println(car.color);
+
 
 
 
